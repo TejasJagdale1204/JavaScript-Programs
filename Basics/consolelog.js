af0e77a8237f5621eb0f90eaba19e16e34a84614
TejasJagdale1204 <tejasjagdale45@gmail.com>
2026-01-08 12:01:46 +0530

    Organized all JavaScript files into proper folders

---
+++ Basics/consolelog.js
@@ -0,0 +1,16 @@
+console.log("Tejas Jagdale");
+console.log("SCOE");
+console.log("Computer Engineering");
+
+let a = 10;
+let b = 15;
+console.log(a + b);
+
+let c = 25;
+console.log(a + c);
+
+console.log("Hi," + " I am from Sangli ....");
+
+let PenPrice = 10;
+let PencilPrice = 25;
+console.log("The total price is : " + (PenPrice+PencilPrice) + " Rupees");
