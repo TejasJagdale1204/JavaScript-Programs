fb5191b24f41013713487ba8872f77abe83d334d
TejasJagdale1204 <tejasjagdale45@gmail.com>
2026-01-18 14:34:25 +0530

    Organized all JavaScript files into proper folders

---
+++ Truthy Falsy/Truthy_Falsy1.js
@@ -0,0 +1,62 @@
+if (true) {
+    console.log("Condition is true");
+}
+else {
+    console.log("Condition is false");
+}
+
+if (false) {
+    console.log("Condition is true");
+}
+else {
+    console.log("Condition is false");
+}
+
+if (0) {
+    console.log("It has true value");
+}
+else {
+    console.log("It has false value");
+}
+
+if (-0) {
+    console.log("It has true value");
+}
+else {
+    console.log("It has false value");
+}
+
+if (" ") {
+    console.log("String is not empty");
+}
+else {
+    console.log("String is empty");
+}
+
+if ("") {
+    console.log("String is not empty");
+}
+else {
+    console.log("String is empty");
+}
+
+if (null) {
+    console.log("It has true value");
+}
+else {
+    console.log("It has false value");
+}
+
+if (undefined) {
+    console.log("It has true value");
+}
+else {
+    console.log("It has false value");
+}
+
+if (10) {
+    console.log("It has true value");
+}
+else {
+    console.log("It has false value");
+}
