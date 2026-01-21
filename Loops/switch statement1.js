91b73af6f0ed28f772e9eecb69f96102f1ed2a97
TejasJagdale1204 <tejasjagdale45@gmail.com>
2026-01-21 22:46:56 +0530

    Organized all JavaScript files into proper folders

---
+++ Loops/switch statement1.js
@@ -0,0 +1,15 @@
+let color = "yellow";
+
+switch (color) {
+    case "red":
+        console.log("stop");
+        break;
+    case "yellow":
+        console.log("slow down");
+        break;
+    case "green":
+        console.log("go");
+        break;
+    default: 
+        console.log("signal is broken");
+}
