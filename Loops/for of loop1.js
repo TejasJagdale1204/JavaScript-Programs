bca501ac0f34bd5abca6b433c30d209013cae165
TejasJagdale1204 <tejasjagdale45@gmail.com>
2026-01-24 16:42:42 +0530

    Organized all JavaScript files into proper folders

---
+++ Loops/for of loop1.js
@@ -0,0 +1,17 @@
+let names = ["Tejas","Shivtej","Ritvik","Prafull","Vishal"];
+for(boys of names) {
+    console.log(boys);
+}
+
+for(letters of "Tejas Jagdale") {
+    console.log(letters);
+}
+
+let list = [ ["A","B","C"] , ["D","E","F"] ];
+for(letters of list) {
+    console.log(letters);
+
+    for(words of letters) {
+        console.log(words);
+    }
+} 
